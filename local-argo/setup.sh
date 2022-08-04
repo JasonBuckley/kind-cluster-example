4c140274e9b7d3fc9e71dac03d54a4fd439a234f
Jason Buckley <jasonbuckleycs@gmail.com>
2022-08-03 22:54:23 -0700

    kind cluster example with argo

---
+++ local-argo/setup.sh
@@ -0,0 +1,14 @@
+# deploy minio
+kubectl create ns minio
+kustomize build minio/ | kubectl -n minio apply -f -
+
+# deploy argo
+kubectl create ns argo
+kustomize build argo-workflows/overlays/local/ | kubectl -n argo apply -f -
+
+# create workflow sa for running workflows
+kubectl apply -f wf-sa.yaml
+
+# deploy argo-events
+kubectl create ns argo-events
+kustomize build argo-events/overlays/local/ | kubectl -n argo-events apply -f -
